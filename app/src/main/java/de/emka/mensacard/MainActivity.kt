19c62fad9c5577fe0a570015eefc911702c7a040
kschluen <kevin.schluensen@wwu.de>
2022-01-22 12:40:08 +0100

    Add current balance on App screen

--- app/src/main/java/de/emka/mensacard/MainActivity.kt
+++ app/src/main/java/de/emka/mensacard/MainActivity.kt
@@ -1,28 +1,22 @@
 package de.emka.mensacard
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import de.emka.mensacard.data.BalanceApi
 import de.emka.mensacard.data.BalanceResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 import android.appwidget.AppWidgetManager
 import android.content.Context
 
 import android.content.Intent
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RemoteViews
-import android.widget.Toast
-import java.math.BigDecimal
+import android.widget.*
 
 
 class MainActivity : AppCompatActivity() {
+    val BASE_URL = "https://api.topup.klarna.com/api/v1/STW_MUNSTER/cards/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,15 +24,50 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPref = getSharedPreferences("cardprefs",Context.MODE_PRIVATE) ?: return
         val nr = sharedPref.getInt("card_nr", -1)
+        val nUrl = "$BASE_URL$nr/"
+
+        val retrofitData = BalanceUtils.getBalanceResponse(nUrl)
 
         if (nr > 0) {
             findViewById<EditText>(R.id.editTextNumber).setText(nr.toString())
+
+            getAndShowBalance(retrofitData)
         }
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            storeNr(findViewById<EditText>(R.id.editTextNumber).text.toString().toInt())
+            val nrB = findViewById<EditText>(R.id.editTextNumber).text.toString().toIntOrNull()
+            if (nrB != null) {
+                storeNr(nrB)
+            }
+            val nUrlB = "$BASE_URL$nrB/"
+            val retrofitDataB = BalanceUtils.getBalanceResponse(nUrlB)
+            getAndShowBalance(retrofitDataB)
             showAppWidget()
         }
+    }
+
+    private fun getAndShowBalance(retrofitData: Call<BalanceResponse>) {
+        retrofitData.enqueue(object : Callback<BalanceResponse?> {
+            @SuppressLint("RemoteViewLayout")
+            override fun onResponse(
+                call: Call<BalanceResponse?>,
+                response: Response<BalanceResponse?>
+            ) {
+                val responseBody = response.body()
+
+                if (responseBody != null) {
+                    val balance = responseBody.balance
+                    val displayText = getString(R.string.currentBalance) + BalanceUtils.intToString(balance)
+                    findViewById<TextView>(R.id.textView2).text = displayText
+                } else {
+                    findViewById<TextView>(R.id.textView2).text = getString(R.string.balanceNotAvailable)
+                }
+            }
+
+            override fun onFailure(call: Call<BalanceResponse?>, t: Throwable) {
+                findViewById<TextView>(R.id.textView2).text = getString(R.string.balanceNotAvailable)
+            }
+        })
     }
 
 
